01c7429d96bcfdf0db590d994d783095ac141e2e
dyabk <dzmitry.yabkevich@outlook.com>
2022-02-02 05:56:38 -0500

    Enabled the display of static information and added a Procfile to use to deploy the app to Heroku

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 morgan.token('sent', (request, response) => JSON.stringify(request.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :sent'))
 
