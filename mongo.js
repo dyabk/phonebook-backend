b11935057d89c3d12166cd96d4053ea801115ea1
dyabk <dzmitry.yabkevich@outlook.com>
2022-02-02 14:21:47 -0500

    Added the check of user command-line arguments to terminate the program if some input is missing

--- mongo.js
+++ mongo.js
@@ -1,1 +1,7 @@
 const mongoose = require('mongoose')
+
+if (process.argv.length < 5) {
+    console.log('Please provide all the arguments as required.')
+    console.log('Correct usage: node mongo.js <password> <name> <phonenumber')
+    process.exit(1)
+}
