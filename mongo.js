2890c1b2bb81ee13df742b02a6be4d1aed50d656
dyabk <dzmitry.yabkevich@outlook.com>
2022-02-02 14:24:46 -0500

    Destructured the array of command-line arguments. Provided the URI for the use by mongoose

--- mongo.js
+++ mongo.js
@@ -5,3 +5,10 @@ if (process.argv.length < 5) {
     console.log('Correct usage: node mongo.js <password> <name> <phonenumber')
     process.exit(1)
 }
+
+const [ , , password, name, number ] = process.argv
+
+const url =
+    `mongodb+srv://majabaza:${password}@cluster0.k9dqp.mongodb.net/person-app?retryWrites=true&w=majority`
+
+mongoose.connect(url)
