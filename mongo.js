11d9a7fb107691505af38e6df46d91400ac4f22d
dyabk <dzmitry.yabkevich@outlook.com>
2022-02-02 15:16:24 -0500

    Prepared the mongo.js application for testing. Included cases when the user provides only the password or the password and more information about the person

--- mongo.js
+++ mongo.js
@@ -1,14 +1,43 @@
 const mongoose = require('mongoose')
+const helper = require('/index')
 
-if (process.argv.length < 5) {
-    console.log('Please provide all the arguments as required.')
-    console.log('Correct usage: node mongo.js <password> <name> <phonenumber')
+const argvLength = process.argv.length
+
+if (argvLength < 3) {
+    console.log('Please provide the required arguments: node mongo.js <password> [<name> <number>]')
     process.exit(1)
+} else {
+    const password = process.argv[2]
+    const personSchema = new mongoose.Schema({
+        id: Number, 
+        name: String,
+        number: String
+    })
+    const Person = mongoose.model('Person', personSchema)
+    
+    if (argvLength === 3) {
+        Person
+            .find({})
+            .then(result => {
+                result.forEach(person => {
+                    console.log(person)
+                })
+            mongoose.connection.close()
+        })
+    }
+    else {
+        const name = process.argv[3]
+        const number = (process.argv.length > 4) ? process.argv[4] : ''
+
+        const person = new Person({
+            id: helper.generateID(),
+            name,
+            number,
+        })
+          
+        person.save().then(result => {
+            console.log(`added ${name} number ${number} to phonebook`)
+            mongoose.connection.close()
+        })
+    }
 }
-
-const [ , , password, name, number ] = process.argv
-
-const url =
-    `mongodb+srv://majabaza:${password}@cluster0.k9dqp.mongodb.net/person-app?retryWrites=true&w=majority`
-
-mongoose.connect(url)
