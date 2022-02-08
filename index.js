ffa4694383822b308d0b1805a40a1e0f28b24da1
dyabk <dzmitry.yabkevich@outlook.com>
2022-02-08 18:46:38 -0500

    Added a method to update an existing person in the database in case if a new record contains the same name

--- index.js
+++ index.js
@@ -96,6 +96,20 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
 const errorHandler = (error, request, response, next) => {
     console.log(error.message)
 
