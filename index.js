af51187caf14ee4c29780d08735f3fa33de2d703
dyabk <dzmitry.yabkevich@outlook.com>
2022-02-08 21:00:49 -0500

    Deleted the hardcoded list of persons for the phonebook and the related functionality, such as generating ids since this is handled by the database

--- index.js
+++ index.js
@@ -12,41 +12,6 @@ app.use(express.json())
 morgan.token('sent', (request, response) => JSON.stringify(request.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :sent'))
 
-let persons = [
-    { 
-        "id": 1,
-        "name": "Arto Hellas", 
-        "number": "040-123456"
-    },
-    { 
-        "id": 2,
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523"
-    },
-    { 
-        "id": 3,
-        "name": "Dan Abramov", 
-        "number": "12-43-234345"
-    },
-    { 
-        "id": 4,
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122"
-    }
-]
-
-const generateID = () => {
-    const newRandom = Math.floor(Math.random() * 4400)
-
-    if (persons
-        .map(person => person.id)
-        .includes(newRandom)) {
-        return generateID()
-    } else {
-        return newRandom
-    }
-}
-
 app.delete('/api/persons/:id', (request, response) => {
     Person.findByIdAndRemove(request.params.id)
         .then(result => {
