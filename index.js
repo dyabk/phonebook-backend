c11d2819965ed250d1defbf174a4652e6c83fdbe
dyabk <dzmitry.yabkevich@outlook.com>
2022-02-11 03:28:17 -0500

    Refactored index.js: replaced the order of the middleware loading and added the unknownEndpoint middleware

--- index.js
+++ index.js
@@ -1,46 +1,36 @@
 require('dotenv').config()
 
-const express = require('express')
 const app = express()
 const cors = require('cors')
+const express = require('express')
 const morgan = require('morgan')
 const Person = require('./models/person')
 
-app.use(cors())
 app.use(express.static('build'))
+app.use(cors())
 app.use(express.json())
 morgan.token('sent', (request, response) => JSON.stringify(request.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :sent'))
 
-app.delete('/api/persons/:id', (request, response) => {
-    Person.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.get('/info', (request, response) => {
+    Person.estimatedDocumentCount().then(result => {
+        response.send(`Phonebook has info for ${result} people<br><br>${Date()}`)
+    })
 })
 
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
         response.json(persons)
-    })
-})
-
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id)
-        .then(person => {
-            if (person) {
-                response.json(person)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
-})
-
-app.get('/info', (request, response) => {
-    Person.estimatedDocumentCount().then(result => {
-        response.send(`Phonebook has info for ${result} people<br><br>${Date()}`)
     })
 })
 
@@ -64,6 +54,26 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+})
+
 app.put('/api/persons/:id', (request, response, next) => {
     const body = request.body
 
@@ -78,16 +88,11 @@ app.put('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-const errorHandler = (error, request, response, next) => {
-    console.log(error.message)
-
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    }
-
-    next(error)
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
 }
-
+  
+app.use(unknownEndpoint)
 app.use(errorHandler)
 
 const PORT = process.env.PORT
